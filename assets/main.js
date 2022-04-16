4595b4bbea6b2213ed769fd289bf70ea1fdb2864
Yehor <egor.chabanov@globeair.com>
2022-04-16 23:33:21 +0300

    add login form, chat box and chat logic

---
+++ assets/main.js
@@ -0,0 +1,56 @@
+const socket        = io()
+const loginContainer    = document.querySelector('#login-container')
+const inputUserName     = document.querySelector('#user_name')
+const chatContainer     = document.querySelector('#chat-container')
+const messages          = document.querySelector('#messages')
+const logoutButton      = document.querySelector('#logout-button')
+const form              = document.querySelector('#message-form')
+const inputMessage      = document.querySelector('#message')
+
+function login() {
+    loginContainer.classList.remove("d-flex");
+    loginContainer.hidden = true;
+
+    chatContainer.hidden = false;
+
+    logoutButton.hidden = false;
+}
+
+function logout() {
+    chatContainer.hidden = true;
+
+    loginContainer.classList.add("d-flex");
+    loginContainer.hidden = false;
+
+    logoutButton.hidden = true;
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    if (inputMessage.value) {
+        socket.emit('chatMessage', {
+            userName: inputUserName.value,
+            message: inputMessage.value
+        })
+
+        inputMessage.value = ''
+    }
+})
+
+socket.on('chatMessage', (data) => {
+    // Create message element
+    const messageItem = document.createElement('div')
+
+    messageItem.classList.add('d-flex',  'flex-row', 'p-3')
+
+    messageItem.innerHTML = `<span class="ml-2 p-3">${data.userName}</span><div class="chat ml-2 p-3">${data.message}</div>`
+
+    // Add new message element
+    messages.appendChild(messageItem)
+
+    // Scroll to the last message
+    // window.scrollTo(0, document.body.scrollHeight)
+    messages.scrollHeight
+})
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+     res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.static(__dirname + '/assets'))
+
+io.on('connection', (socket) => {
+     socket.on('chatMessage', (data) => {
+          io.emit('chatMessage', {
+               userName: data.userName,
+               message: data.message
+          })
+     })
+})
+
+http.listen(4000, () => {
+     console.log('Server is running')
+})
